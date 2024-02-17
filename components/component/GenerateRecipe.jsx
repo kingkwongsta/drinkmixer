436ed8196bbde58837dffedbf0c7c166ff861e15
Kwongster <bryant.j.kwong@gmail.com>
2024-02-16 21:51:22 -0800

    Add function to fetch and display recipe

--- components/component/GenerateRecipe.jsx
+++ components/component/GenerateRecipe.jsx
@@ -1,5 +1,41 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import userStore from "@/lib/userStore";
+import { createCompletion } from "@/app/actions";
 
 export default function GenerateRecipe() {
-  return <Button className="border-2 bg-slate-300">Generate Recipe</Button>;
+  const { drinkRecipe, setDrinkRecipe, userFlavor, userLiquor, userMood } =
+    userStore();
+  async function getRecipe() {
+    // setIsLoading(true);
+    try {
+      const response = await createCompletion(userFlavor, userLiquor, userMood);
+      if (response) {
+        setDrinkRecipe(response);
+      } else {
+        console.log("recipe unavailable");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      //   setIsLoading(false);
+    }
+  }
+  return (
+    <>
+      <form action={getRecipe}>
+        <Button type="submit" className="border-2 bg-slate-300">
+          Generate Recipe
+        </Button>
+      </form>
+      <button
+        onClick={() => {
+          console.log(drinkRecipe);
+        }}
+        className="border-2 border-cyan-500"
+      >
+        Get Recipe
+      </button>
+    </>
+  );
 }
