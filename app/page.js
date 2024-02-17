431b96b73e41cd0bf19c492a3310b3e35892f807
Kwongster <bryant.j.kwong@gmail.com>
2024-02-16 19:46:59 -0800

    Add Dropdown component to Home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Dropdown from "@/components/component/dropdown";
 
 export default function Home() {
   return (
     <main>
       <div>HELLO</div>
+      <Dropdown />
     </main>
   );
 }
