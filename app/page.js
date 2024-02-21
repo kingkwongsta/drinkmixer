0437414e1f51699d061d82db9b5dae22ce508fe9
Kwongster <bryant.j.kwong@gmail.com>
2024-02-20 19:42:34 -0800

    Add ThemeToggle component to Home page

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import GenerateRecipe from "@/components/component/GenerateRecipe";
 import RecipeCard from "@/components/component/RecipeCard";
 import { RecipeCard2 } from "@/components/component/RecipeCard2";
 import userStore from "@/lib/userStore";
+import { ThemeToggle } from "@/components/component/ThemeToggle";
 
 export default function Home() {
   // create a new user store
@@ -13,6 +14,7 @@ export default function Home() {
   const { drinkRecipe } = userStore();
   return (
     <main className="w-full px-12 py-12 md:py-24 space-y-[50px]">
+      <ThemeToggle />
       <Title />
       <Dropdown />
       <div className="flex justify-center">
