ab5b78fc90695a06c0fc237b20a0d409c3ed2ce4
Kwongster <bryant.j.kwong@gmail.com>
2024-02-23 23:10:03 -0800

    firebase init

--- components/title.jsx
+++ components/title.jsx
@@ -1,7 +1,9 @@
 "use client";
 import userStore from "@/lib/userStore";
+
 export default function Title() {
   const { setDrinkRecipe } = userStore();
+
   return (
     <div className="flex flex-col gap-2 items-center justify-center text-center">
       <div className="grid gap-8">
@@ -12,8 +14,8 @@ export default function Title() {
           Cocktail Recipe Generator
         </h1>
         <p className="text-gray-500 dark:text-gray-400">
-          Let's mix up something special! Select your preferences and we'll give
-          you a unique cocktail recipe.
+          Let&#39;s mix up something special! Select your preferences and
+          we&#39;ll give you a unique cocktail recipe.
         </p>
       </div>
     </div>
