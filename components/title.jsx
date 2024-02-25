d6a00df93e0f04f969a644a62a1d89956dbeddd4
Kwongster <bryant.j.kwong@gmail.com>
2024-02-24 16:55:05 -0800

    Update title to "Drink Masters" and refine description

--- components/title.jsx
+++ components/title.jsx
@@ -11,11 +11,10 @@ export default function Title() {
           onClick={() => setDrinkRecipe("")}
           className="text-3xl font-bold tracking-tight"
         >
-          Cocktail Recipe Generator
+          Drink Masters
         </h1>
         <p className="text-gray-500 dark:text-gray-400">
-          Let&#39;s mix up something special! Select your preferences and
-          we&#39;ll give you a unique cocktail recipe.
+          Discover a world of exquisite cocktails by sharing your preferences
         </p>
       </div>
     </div>
